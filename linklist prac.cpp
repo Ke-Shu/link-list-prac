d8e6cb693a15f946bc652fec9989fa91dd015669
Ke-Shu <javkelbubb@gmail.com>
2025-10-23 16:00:52 -0400

    Add files via upload

---
+++ linklist prac.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+//create the linked list called Lk_List
+struct Lk_List {
+    // the Lk_List
+   int data;  //data 
+   struct Lk_List *next; // the link or pointer
+};
+struct Lk_List *head = NULL;// the head Lk_List 
+struct Lk_List *current = NULL;// current Lk_List
+
+//insertion at the beginning
+void insertAtBegin(int data){
+   
+   //create a link
+   struct Lk_List *link = (struct Lk_List*) malloc(sizeof(struct Lk_List));
+   link->data = data;
+   
+   // point it to old first Lk_List
+   link->next = head;
+   
+   //point first to new first Lk_List
+   head = link;
+}
+//insertion at the end
+void insertAtEnd(int data){
+   
+   //create a link
+   struct Lk_List *link = (struct Lk_List*) malloc(sizeof(struct Lk_List));
+   link->data = data;
+   struct Lk_List *currentLk_List = head;
+
+   // point it to old first Lk_List
+   while(currentLk_List->next != NULL)
+      currentLk_List = currentLk_List->next;
+
+   //point first to new first Lk_List
+   currentLk_List->next = link;
+}
+//insertafter node 
+void insertAfterNode(struct  Lk_List *list, int data){
+   struct  Lk_List *link = (struct  Lk_List*) malloc(sizeof(struct  Lk_List));
+   link->data = data;
+   link->next = list->next;
+   list->next = link;
+}
+// display the list
+void printList(){
+   struct Lk_List *link = head;//start from the beginning
+   cout << "\n[";
+   
+   //while there is still a Lk_List print the data
+   while(link != NULL) {
+      cout << " " << link->data << " ";
+      link = link->next;
+   }
+   cout << "]";
+}
+
+
+int main(){
+
+    int a1,a2,a3,a4,a5,a6,a7;
+
+cout << "Enter numbers 1-7: \n";
+cin >> a1 >> a2 >> a3 >> a4 >> a5 >> a6 >> a7;
+
+   
+   insertAtBegin(1);        
+   insertAtEnd(3);          
+   insertAtEnd(4);          
+   insertAtBegin(2);        
+
+   
+   struct Lk_List *ptr = head; 
+   insertAfterNode(ptr, 5);    
+   insertAfterNode(ptr->next, 6); 
+   insertAfterNode(ptr->next->next, 7); 
+
+   printList();
+
+    return 0;
+}
